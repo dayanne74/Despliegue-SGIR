4f18270324b85518cbaa32675a580ac80fda02e6
Dayanne M <dayanne.moque@soy.sena.edu.co>
2025-06-14 18:29:09 -0500

    Fix: corrige vite.config y bloqueo de versiones

--- Proyecto-SGIR/REACT/react/vite.config.js
+++ Proyecto-SGIR/REACT/react/vite.config.js
@@ -3,20 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["mui-datatables"]
-    }
-  },
   server: {
     proxy: {
-      // Proxy para llamadas API
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
-      // Proxy para imágenes estáticas
       '/uploads': {
         target: 'http://localhost:5000',
         changeOrigin: true,
@@ -24,8 +17,9 @@ export default defineConfig({
       },
     },
   },
-  // Puedes agregar configuración MIME aquí, pero fuera del bloque `server`
-  mimeTypes: {
-    'application/javascript': ['.js', '.jsx'],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    
   },
 });
